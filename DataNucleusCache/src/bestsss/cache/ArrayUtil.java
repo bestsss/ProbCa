fb570831e66da0d5c07e42f90f4a3a63b08ced7a
bestsss <stanimir.simeonoff@gmail.com>
2015-03-15 21:45:35 +0200

    identation

--- DataNucleusCache/src/bestsss/cache/ArrayUtil.java
+++ DataNucleusCache/src/bestsss/cache/ArrayUtil.java
@@ -9,8 +9,8 @@ import org.datanucleus.cache.CachedPC;
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
- /**
-  * @author Stanimir Simeonoff
+/**
+ * @author Stanimir Simeonoff
  */
 class ArrayUtil {
   private static final int CLASS = 1;
@@ -18,19 +18,19 @@ class ArrayUtil {
   private static final int HITS = 3;
   private static final int TIME = 4;
   static final int RESERVED = TIME;
-  
+
   static Object[] newArray(CachedPC<?> pc, int length, Allocator allocator, int time) {
     length += RESERVED;
     Object[] fields = allocator.get(length);
     length = fields.length;
-    
+
     fields[length-CLASS] = pc.getObjectClass();
     fields[length-VERSION] = pc.getVersion();
     fields[length-HITS] = IntegerProvider.ZERO;
     fields[length-TIME] = IntegerProvider.get(time);    
     return fields;
   }
-  
+
   static Object[] extend(Object[] allFields, int currentLength, int index) {
     int len = Allocator.length( index+(1+4+RESERVED));//4 extra bytes
     Object[] copy = Arrays.copyOf(allFields, len);
@@ -42,13 +42,13 @@ class ArrayUtil {
     }
     return copy;
   }
-  
+
   static int maxLength(Object[] allFields) {
     return allFields.length - RESERVED;
   }
   static void incHitCount(Object[] fields){
     final int idx = fields.length - HITS;
-  Object n = fields[idx];
+    Object n = fields[idx];
     if (n instanceof Integer){
       n = IntegerProvider.get( ((Integer)n)+1);//this requires CAS not to miss some elements but we can live with non-precise
     } else{ 
@@ -56,11 +56,11 @@ class ArrayUtil {
     }
     fields[idx] = n;  
   }
-  
+
   static void setTimeAndHitCount(Object[] fields, int time){
     //increase hit count 
     //and set time    
-  incHitCount(fields);
+    incHitCount(fields);
     fields[fields.length - TIME] = IntegerProvider.get(time);
   }
 
@@ -72,6 +72,6 @@ class ArrayUtil {
     return (Integer) o1[o1.length - HITS];
   }
   public static Class<?> getClass(Object[] o1) {
-  return (Class<?>)o1[o1.length - CLASS];
+    return (Class<?>)o1[o1.length - CLASS];
   }
 }
